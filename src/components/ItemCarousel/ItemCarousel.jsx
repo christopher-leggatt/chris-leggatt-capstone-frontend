cdc7849fbaba74eb259182d7128e525d32723ca4
Chris Leggatt <twoshow1978@gmail.com>
2023-09-04 13:59:51 -0400

    created state folder; added cart menu logic wip; added provider to index.js

---
+++ src/components/ItemCarousel/ItemCarousel.jsx
@@ -0,0 +1,12 @@
+import './_ItemCarousel.scss';
+import React from 'react';
+
+const ItemCarousel = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ItemCarousel;
--- src/index.js
+++ src/index.js
@@ -3,15 +3,22 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { ThemeProvider } from "@mui/material/styles";
-import { CssBaseline } from "@mui/material";
 import theme from "./styles/themes/theme";
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+import cartReducer from './state';
+
+const store = configureStore({
+  reducer: { cart: cartReducer },
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <Provider store={store}>
     <ThemeProvider theme={theme}>
-      {/* <CssBaseline /> */}
       <App />
     </ThemeProvider>
+    </Provider>
   </React.StrictMode>
 );
